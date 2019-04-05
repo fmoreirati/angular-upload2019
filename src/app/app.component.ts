889a676a7f3291eb327222720ffaf58acac1ade8
fmoreirati <fabianomoreira.ti@gmail.com>
2019-04-05 11:56:21 -0300

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  onSelectFile: File = null;
 }
 //http://www.fabricadecodigo.com/rest-api-ionic/
+//https://www.youtube.com/watch?v=YkvqLNcJz3Y
