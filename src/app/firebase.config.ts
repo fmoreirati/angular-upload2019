f693956f5b08e608d130d77a109588e71de868b2
fmoreirati <fabianomoreira.ti@gmail.com>
2019-04-07 20:51:17 -0300

    1 file added

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE =  {
+    apiKey: "AIzaSyBBQvs14EVmw4HHY8WPBjKi-OMmaPWSzds",
+    authDomain: "cadclientes2019.firebaseapp.com",
+    databaseURL: "https://cadclientes2019.firebaseio.com",
+    projectId: "cadclientes2019",
+    storageBucket: "cadclientes2019.appspot.com",
+    messagingSenderId: "258663233815"
+  };
