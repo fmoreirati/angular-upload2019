4fe9e8118b69bb5146523785141094325f7d9556
fmoreirati <fabianomoreira.ti@gmail.com>
2019-04-07 21:50:37 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { FIREBASE } from './firebase.config';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AngularFireModule.initializeApp(FIREBASE)],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
